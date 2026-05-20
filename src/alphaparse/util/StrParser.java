5401e9fc702d98131f469917954c0e03f80a5ae3
ArminKleinert <armin.kleinert@t-online.de>
2026-05-21 00:24:11 +0200

    More compact StrParser.

--- src/alphaparse/util/StrParser.java
+++ src/alphaparse/util/StrParser.java
@@ -8,143 +8,54 @@ import java.util.regex.Pattern;
  * Helper functions for creating parsers from strings.
  */
 public final class StrParser {
-    final @NotNull StringBuilder sb = new StringBuilder();
+    final @NotNull StringBuilder sb;
 
+    /**
+     * The constructor initializes a StringBuilder or similar object for internal use.
+     */
     public StrParser() {
+        sb = new StringBuilder();
     }
 
     // Converts a single-quoted string to a double-quoted one.
-    private @NotNull StringBuilder escape(final @NotNull CharSequence s) {
+    private @NotNull String prepare(final int offset, final char starter, final @NotNull String s) {
         sb.setLength(0);
-        for (int i = 0; i < s.length(); i++) {
-            final char c = s.charAt(i);
+        for (int i = offset; i < s.length() - 1; i++) {
+            char c = s.charAt(i);
+
+            if (c == starter) {
+                break;
+            }
+
             if (c == '\\') {
                 if (i + 1 >= s.length())
                     throw new IllegalArgumentException("Encountered backslash character at end of string: " + s);
-                final char c2 = s.charAt(i + 1);
+
                 i++;
-                if (c2 != '\'') sb.append(c);
-                sb.append(c2);
-            } else if (c == '"') {
-                sb.append('\\').append('"');
+                final char c2 = s.charAt(i);
+
+                if (c2 == starter) {
+                    sb.append(c2);
+                } else {
+                    switch (c2) {
+                        case 'b' -> sb.append('\b');
+                        case 'f' -> sb.append('\f');
+                        case 'n' -> sb.append('\n');
+                        case 'r' -> sb.append('\r');
+                        case 't' -> sb.append('\t');
+                        case 'u' -> {
+                            i += 5;
+                            sb.appendCodePoint( Integer.parseInt(s, i - 4, i, 16));
+                        }
+                        default -> sb.append('\\').append( c2);
+                    }
+                }
             } else {
                 sb.append(c);
             }
         }
-        //sb.append('"');
-        return sb;
-    }
 
-    /**
-     * Unescapes a string.
-     *
-     * @param inputCharSequence The input string.
-     * @return An unescaped string.
-     */
-    public static @NotNull String parse(final @NotNull StringBuilder inputCharSequence) {
-        final int inputLength = inputCharSequence.length();
-        long lenAndCh;
-
-        try {
-            for (int i = 0; i < inputLength; i++) {
-                int ch = inputCharSequence.charAt(i);
-
-                if (ch == '"')
-                    break;
-
-                if (ch == '\\') {
-                    i++;
-                    if (i >= inputLength)
-                        throw new ArrayIndexOutOfBoundsException();
-                    ch = inputCharSequence.charAt(i);
-
-                    switch (ch) {
-                        case '"':
-                        case '\\':
-                            break;
-                        case 'b':
-                            ch = '\b';
-                            break;
-                        case 'f':
-                            ch = '\f';
-                            break;
-                        case 'n':
-                            ch = '\n';
-                            break;
-                        case 'r':
-                            ch = '\r';
-                            break;
-                        case 't':
-                            ch = '\t';
-                            break;
-                        case 'u':
-                            i++;
-                            ch = inputCharSequence.charAt(i);
-                            if (Character.digit(ch, 16) == -1) {
-                                throw new IllegalArgumentException("Invalid unicode escape: \\u" + (char) ch);
-                            }
-
-                            lenAndCh = readUnicodeChar(inputCharSequence, i, ch, 16, 4, true);
-                            ch = (int) lenAndCh;
-                            i += (int) (lenAndCh >> 32) - 1;
-                            break;
-                        default:
-                            if (!Character.isDigit(ch)) {
-                                throw new IllegalArgumentException("Unsupported escape character: \\" + (char) ch);
-                            }
-
-                            lenAndCh = readUnicodeChar(inputCharSequence, i, ch, 8, 3, false);
-                            ch = (int) lenAndCh;
-                            i += (int) (lenAndCh >> 32) - 1;
-                            if (ch > 255) {
-                                throw new IllegalArgumentException("Octal escape sequence must be in range [0, 377].");
-                            }
-                            break;
-                    }
-                }
-
-                inputCharSequence.appendCodePoint(ch);
-            }
-        } catch (ArrayIndexOutOfBoundsException exception) {
-            throw new IllegalArgumentException("EOF while reading string",exception);
-        }
-
-        return inputCharSequence.substring(inputLength);
-    }
-
-    private static long readUnicodeChar(final @NotNull CharSequence sb,
-                                        int indexInCharSequence,
-                                        final int initChar,
-                                        final int numericBase,
-                                        final int expectedLength,
-                                        final boolean requireExactLength) {
-        int outputNumber = Character.digit(initChar, numericBase);
-        int parsedLength = 1;
-        if (outputNumber == -1) {
-            throw new IllegalArgumentException("Invalid digit: " + (char) initChar);
-        } else {
-            indexInCharSequence++;
-            for (; parsedLength < expectedLength; parsedLength++, indexInCharSequence++) {
-                final char ch = sb.charAt(indexInCharSequence);
-                if (Character.isWhitespace(ch)) {
-                    parsedLength--;
-                    break;
-                }
-
-                final int d = Character.digit(ch, numericBase);
-                if (d == -1) {
-                    throw new IllegalArgumentException("Invalid digit: " + ch);
-                }
-
-                outputNumber = outputNumber * numericBase + d;
-            }
-
-            if (parsedLength != expectedLength && requireExactLength) {
-                throw new IllegalArgumentException("Invalid character length: " + parsedLength + ", should be: " + expectedLength);
-            } else {
-                return ((long) parsedLength <<32) + outputNumber;
-            }
-        }
+        return sb.toString();
     }
 
     /**
@@ -154,10 +65,8 @@ public final class StrParser {
      * @param s The input string.
      * @return A string.
      */
-    public @NotNull String processString(final @NotNull CharSequence s) {
-        final @NotNull CharSequence stripped = s.subSequence(1, s.length() - 1);
-        final @NotNull StringBuilder removeEscapedSingleQuotes = escape(stripped);
-        return parse(removeEscapedSingleQuotes.append('"'));
+    public @NotNull String processString(final @NotNull String s) {
+        return prepare(1, s.charAt(0), s);
     }
 
     /**
@@ -167,9 +76,7 @@ public final class StrParser {
      * @param s The input string.
      * @return A regex.
      */
-    public @NotNull Pattern processRegexp(final @NotNull CharSequence s) {
-        final @NotNull CharSequence stripped = s.subSequence(2, s.length() - 1);
-        final @NotNull StringBuilder removeEscapedSingleQuotes = escape(stripped);
-        return Pattern.compile(removeEscapedSingleQuotes.toString());
+    public @NotNull Pattern processRegexp(final @NotNull String s) {
+        return Pattern.compile(prepare(2, s.charAt(1), s));
     }
 }
